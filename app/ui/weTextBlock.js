f9c9b5afdcce5f5b440632e8261b8711e875ddd4
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-01-25 13:45:57 -0600

    adjust we text block margin

--- app/ui/weTextBlock.js
+++ app/ui/weTextBlock.js
@@ -2,7 +2,7 @@ import MMMLineLogoBlack from "./mmmLineLogoBlack";
 
 export default function WeTextBlock({ headline, text }) {
   return (
-    <div className="flex flex-col justify-center items-center h-full p-12 -mb-12 -mb-0">
+    <div className="flex flex-col justify-center items-center h-full p-12 lg:-mb-0">
       <div className="flex flex-col justify-between h-full shadow-2xl">
         <div>
           <h4 className="font-bold mr-auto p-4 -mb-4">{headline}</h4>
