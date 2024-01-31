4fcb302d5aa7576c677ae80a82dc7ab912beeaf2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-01-31 11:08:19 -0600

    banner vid padding photo, livestream

--- app/ui/bannerVideo.js
+++ app/ui/bannerVideo.js
@@ -10,7 +10,7 @@ export default function BannerVideo({src, title}) {
       <div className="relative flex justify-center">
         <div className="w-full banner-video-aspect-ratio-container"
         style={{
-          paddingTop: title === 'MMM Livestreaming Video' || title==="MMM Photography Slide Show" ? '42.5%' : '56.25%',
+          paddingTop: title === 'MMM Livestreaming Video' || title==="MMM Photography Slide Show" ? '42%' : '56.25%',
         }}
         > 
           <iframe
