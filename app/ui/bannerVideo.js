96c4dc563b329eab8c98513ea1c8f16d52572738
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-01-31 11:38:29 -0600

    banner vid conditional padding

--- app/ui/bannerVideo.js
+++ app/ui/bannerVideo.js
@@ -1,27 +1,26 @@
-
 import MMMLineLogoBlack from "./mmmLineLogoBlack";
 
-
-export default function BannerVideo({src, title}) {
-
-
+export default function BannerVideo({ src, title }) {
   return (
     <section className="grid grid-cols-1 gap-4">
       <div className="relative flex justify-center">
-        <div className="w-full banner-video-aspect-ratio-container"
-        style={{
-          paddingTop: title === 'MMM Livestreaming Video' || title==="MMM Photography Slide Show" ? '42.25%' : '56.25%',
-        }}
-        > 
+        <div
+          className="w-full banner-video-aspect-ratio-container"
+          style={{
+            paddingTop:
+              title === "MMM Livestreaming Video"
+                ? "42.5%"
+                : title === "MMM Photography Slide Show"
+                ? "42.25%"
+                : "56.25%",
+          }}>
           <iframe
             title={title}
             src={src}
             allow="autoplay; fullscreen; picture-in-picture"
-            className="absolute top-0 left-0 w-full h-full" 
-          ></iframe>
+            className="absolute top-0 left-0 w-full h-full"></iframe>
         </div>
       </div>
     </section>
   );
-  
 }
