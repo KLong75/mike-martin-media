ef3767a0dc7441a0eb0b12fe05d5b46c79f7306f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-02-02 19:44:44 -0600

    refactor back to our work button

--- app/ui/backToOurWorkLink.js
+++ app/ui/backToOurWorkLink.js
@@ -6,7 +6,7 @@ import { IoIosArrowBack } from "react-icons/io";
 
 export default function BackToOurWorkLink() {
   return (
-    <Link href="/mmm_video">
+    <Link href="/our_work">
       <div className="ml-2 flex">
         <IoIosArrowBack className="text-3xl "/>
         <span className="text-xs mt-2 -ml-1 md:text-sm md:mt-1.5">Back to Our Work</span>
