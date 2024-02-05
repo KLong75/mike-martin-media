d6988eacb83e3d46bb650065430b904b8bcfc67a
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-02-05 16:47:37 -0600

    fix videoData for livestream page

--- app/ui/videoGallery.js
+++ app/ui/videoGallery.js
@@ -6,26 +6,9 @@ import { videoData } from "../lib/data";
 
 export default function VideoGallery( {selectedCategory}) {
 
-  const allVideosExceptLivestream = videoData.filter((video) => video.category !== "Livestreaming");
-  // return (
-  //     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0">
-  //       {videoData
-  //       .filter((video) => video.category === selectedCategory)
-  //       .map((video) => (
-  //         <div key={video.client} className="text-white w-full h-auto hover:transform hover:scale-110 hover:z-40 hover:text-black transition-transform">
-  //           <ClientVideoFrame
-  //             src={video.src}
-  //             client={video.client}
-  //             category={video.category}
-  //             thumbnail_src={video.thumbnail_src}
-  //           />
-  //         </div>
-  //       ))}
-  //     </div>
-  // );
   return (
     <div className={`grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 ${selectedCategory === 'Livestreaming' ? 'lg:grid-cols-2' : 'lg:grid-cols-3'} gap-0`}>
-      {allVideosExceptLivestream
+      {videoData
         .filter((video) => selectedCategory === "All Videos" || video.category === selectedCategory)
         .map((video) => (
           <div key={video.client} className="text-white w-full h-auto hover:transform hover:scale-110 hover:z-40 hover:text-black transition-transform">
