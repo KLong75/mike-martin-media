caa798e3f3f0aa948ebc181595fc9ffb3f65a6eb
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-02-07 17:33:55 -0600

    preconnect to vimeo

--- app/ui/bannerVideo.js
+++ app/ui/bannerVideo.js
@@ -1,26 +1,36 @@
-import MMMLineLogoBlack from "./mmmLineLogoBlack";
+// import from next
+import Head from "next/head";
 
 export default function BannerVideo({ src, title }) {
   return (
-    <section className="grid grid-cols-1 gap-4">
-      <div className="relative flex justify-center">
-        <div
-          className="w-full banner-video-aspect-ratio-container"
-          style={{
-            paddingTop:
-              title === "MMM Livestreaming Video"
-                ? "42.2%"
-                : title === "MMM Photography Slide Show"
-                ? "42.2%"
-                : "56.25%",
-          }}>
-          <iframe
-            title={title}
-            src={src}
-            allow="autoplay; fullscreen; picture-in-picture"
-            className="absolute top-0 left-0 w-full h-full"></iframe>
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://f.vimeocdn.com"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <section className="grid grid-cols-1 gap-4">
+        <div className="relative flex justify-center">
+          <div
+            className="w-full banner-video-aspect-ratio-container"
+            style={{
+              paddingTop:
+                title === "MMM Livestreaming Video"
+                  ? "42.2%"
+                  : title === "MMM Photography Slide Show"
+                  ? "42.2%"
+                  : "56.25%",
+            }}>
+            <iframe
+              title={title}
+              src={src}
+              allow="autoplay; fullscreen; picture-in-picture"
+              className="absolute top-0 left-0 w-full h-full"></iframe>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
