d323817c60e75472d0439a580538c97f7b5fd11b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-02-08 07:50:37 -0600

    now viewing video category banner spacing

--- app/our_work/page.js
+++ app/our_work/page.js
@@ -62,18 +62,6 @@ export default function Page() {
         <div>
           <OurWorkTiles />
         </div>
-        {/* <section className="-mb-6">
-      <VideoCategories onCategorySelected={setSelectedCategory}/>
-      </section>
-      <VideoGallery selectedCategory={selectedCategory} /> */}
-      {/* <div className="flex justify-center items-center p-2 mb-6 mt-6">
-          <MmmSiteButtonTextOnly
-            href="/contact"
-            label="Contact Us"
-            backgroundColor="bg-black"
-            textColor="text-white"
-          />
-        </div> */}
         <div className=" text-sm md:text-base -mt-6 md:mt-6">
           <TextSegment text="From Corporate, Education, Medical and Nonprofit videos, to Livestreaming, and Photography, we are your partner through every step of the process. Contact us today to get started on your project. This text can change. What we need here is an invitation for visitors to contact MMM." />
         </div>
--- app/ui/videoCategories.js
+++ app/ui/videoCategories.js
@@ -8,7 +8,6 @@ import { HiGlobeAlt } from "react-icons/hi2";
 import { FaVideo } from "react-icons/fa6";
 import { FaKitMedical } from "react-icons/fa6";
 
-
 export default function VideoCategories({ onCategorySelected }) {
   const [selectedCategory, setSelectedCategory] = useState("All Videos");
   const handleCategorySelected = (category) => {
@@ -24,35 +23,39 @@ export default function VideoCategories({ onCategorySelected }) {
     { name: "Nonprofit", icon: <HiGlobeAlt size={24} /> },
   ];
 
-  const selectedCategoryObject = categoryOptions.find(category => category.name === selectedCategory);
+  const selectedCategoryObject = categoryOptions.find(
+    (category) => category.name === selectedCategory
+  );
 
   return (
     <section>
-      <h3 className="text-lg font-bold text-center -mb-6">Select a category:</h3>
+      <h3 className="text-lg font-bold text-center -mb-6">
+        Select a category:
+      </h3>
       <ul className="flex flex-wrap justify-center gap-4 p-6">
         {categoryOptions.map((category) => (
           <li
             key={category.name}
             onClick={() => handleCategorySelected(category.name)}
             className={`text-sm flex flex-col items-center justify-center relative transition-transform cursor-pointer m-3 ${
-              selectedCategory === category.name ? "font-bold scale:110" : "text-black hover:transform hover:scale-110 hover:rotate-3 hover:font-medium"
-            }`}
-          >
+              selectedCategory === category.name
+                ? "font-bold scale:110"
+                : "text-black hover:transform hover:scale-110 hover:rotate-3 hover:font-medium"
+            }`}>
             {category.icon}
             <span className="mt-2">{category.name}</span>
           </li>
         ))}
       </ul>
       <div className="bg-black text-2xl text-white grid grid-cols-1 w-full h-24 md:h-28 text-center -mt-4">
-      <div>
         <h4 className="font-bold text-xl mt-1 ">Currently Viewing:</h4>
-        <div className="flex  justify-center items-center mt-1">
-        {selectedCategoryObject && selectedCategoryObject.icon}
+        <div className="flex  justify-center items-center ">
+          {selectedCategoryObject && selectedCategoryObject.icon}
         </div>
-        {selectedCategory === 'All Videos' ? selectedCategory : `${selectedCategory} Videos`}
-        </div>
+        {selectedCategory === "All Videos"
+          ? selectedCategory
+          : `${selectedCategory} Videos`}
       </div>
     </section>
   );
 }
-
