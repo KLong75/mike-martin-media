25d6c78c9e8ddfd3a8c53b16f50a43004078307e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-02-25 17:23:00 -0600

    create video logo fade effect component

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -1,5 +1,6 @@
 // import from next
 import Image from "next/image";
+import Head from "next/head";
 // import from react
 import { useState, useEffect } from "react";
 import MMMLineLogoBlack from "./mmmLineLogoBlack";
@@ -11,10 +12,10 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
   useEffect(() => {
     const hideLogoTimer = setTimeout(() => {
       setHideLogo(true);
-    }, 4000);
+    }, 800);
     const videoFadeTimer = setTimeout(() => {
       setShowVideo(true);
-    }, 500);
+    },0);
     return () => {
       clearTimeout(hideLogoTimer);
       clearTimeout(videoFadeTimer);
@@ -22,42 +23,66 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
   }, []);
 
   return (
-    <section className="grid grid-cols-1 gap-4 ">
-      <div className="relative flex justify-center">
-        <div
-          className="w-full banner-video-aspect-ratio-container"
-          style={{
-            paddingTop:
-              title === "MMM_Livestreaming_Video" ||
-              title === "MMM_Our_Work_Video"
-                ? "42.2%"
-                : title === "MMM_Photography_Slide_Show"
-                ? "42.2%"
-                : "56.25%",
-          }}>
-          <iframe
-            title={title}
-            src={src}
-            allow="autoplay; fullscreen; picture-in-picture"
-            className="absolute top-0 left-0 w-full h-full"></iframe>
+    <>
+      <Head>
+        <link
+          rel="preconnect"
+          href="https://player.vimeo.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://i.vimeocdn.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preconnect"
+          href="https://f.vimeocdn.com"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <section className="grid grid-cols-1 gap-4 ">
+        <div className="relative flex justify-center">
+          <div
+            className={`banner-video-aspect-ratio-container w-full transition-all duration-8000 ease-in-out ${
+              showVideo ? "opacity-100" : "opacity-0"
+            }`}
+            style={{
+              paddingTop:
+                title === "MMM_Livestreaming_Video" ||
+                title === "MMM_Our_Work_Video"
+                  ? "42.2%"
+                  : title === "MMM_Photography_Slide_Show"
+                  ? "42.2%"
+                  : "56.25%",
+            }}>
+            <iframe
+              title={title}
+              src={src}
+              allow="autoplay; fullscreen; picture-in-picture"
+              className="absolute top-0 left-0 w-full h-full"></iframe>
+          </div>
+          {/* Logo */}
+          <div
+            className={`mt-12 sm:mt-24 lg:mt-28 xl:mt-32 2xl:mt-52 w-72 sm:w-100 md:w-144 lg:w-200 xl:w-400 2xl:w-600 justify-center items-center absolute transition-opacity duration-8000 ease-in-out ${
+              hideLogo ? "opacity-0" : "opacity-100"
+            }`}>
+            <Image
+              priority
+              src="/branding/black_mmm_large_crop_resize.png"
+              width={1642}
+              height={560}
+              alt="Mike Martin Media logo"
+            />
+            <h1 className="text-center text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-bold mt-4">
+              MIKE MARTIN MEDIA
+            </h1>
+          </div>
         </div>
-        {/* Logo */}
-        <div
-          className={`absolute -mt-14 transition-opacity duration-8000 ease-in-out ${
-            hideLogo ? "opacity-0" : "opacity-100"
-          }`}>
-          <Image
-            priority
-            src="/branding/black_mmm_large.png"
-            width={816}
-            height={612}
-            alt="Mike Martin Media logo"
-          />
-        </div>
-      </div>
-      {/* <div className="-mt-4 lg:-mt-12">
+        {/* <div className="-mt-4 lg:-mt-12">
     <MMMLineLogoBlack />
     </div> */}
-    </section>
+      </section>
+    </>
   );
 }
