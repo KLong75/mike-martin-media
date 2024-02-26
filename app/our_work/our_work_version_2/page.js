b8d8646e717a9cbac31fe51538206b9532e3b32b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-02-25 18:06:00 -0600

    our work page version 2

--- app/our_work/our_work_version_2/page.js
+++ app/our_work/our_work_version_2/page.js
@@ -7,14 +7,12 @@ import Image from "next/image";
 // import components
 // import VideoGallery from "../ui/videoGallery";
 // import VideoCategories from "../ui/videoCategories";
-import TextSegment from "../ui/textSegment";
-import OurWorkTiles from "../ui/ourWorkTiles";
-import BackToHomeLink from "../ui/backToHome";
-import MMMLineLogoBlack from "../ui/mmmLineLogoBlack";
-import BannerVideo from "../ui/bannerVideo";
-import BannerImage from "../ui/bannerImage";
-import ContactUsButton from "../ui/contactUsButton";
-import WorkCategorySection from "../ui/workCategorySection";
+
+import MMMLineLogoBlack from "../../ui/mmmLineLogoBlack";
+import BannerVideo from "../../ui/bannerVideo";
+import BannerImage from "../../ui/bannerImage";
+import ContactUsButton from "../../ui/contactUsButton";
+import WorkCategorySection from "../../ui/workCategorySection";
 // import icons
 import { TbDrone } from "react-icons/tb";
 import { FaVideo, FaCameraRetro } from "react-icons/fa6";
@@ -37,10 +35,10 @@ export default function Page() {
       <main>
         <div className="-mt-1">
           <BannerImage
-            src="/images/ourWorkPage/our_work_banner_image.jpg"
+            src="/images/ourWorkPage/our_work_video_banner_image.png"
             alt="MMM Video Production at work"
-            width={5008}
-            height={1996}
+            width={4320}
+            height={1584}
             priority={true}
           />
         </div>
@@ -50,9 +48,11 @@ export default function Page() {
           </h3>
         </div>
         <div className="md:text-lg flex justify-center items-center mt-6 mb-6">
-          <p className="text-center text-balance w-10/12 p-2">
-            From corporate, education, medical, or nonprofit videos, to livestreaming, drone work, and photography, MMM is your
-            partner through every step of the process. Check out work below and
+          <p className="md:text-xl text-center text-balance w-10/12 p-2">
+            From corporate, education, medical, or nonprofit videos, to
+            livestreaming, drone work, and photography, MMM is your partner
+            through every step of the process. We are known for our award winning video production services but we have more than one way to get you what you need. Let us know if
+            one of our other services fits your needs. Check out work below and
             <Link href="/contact">
               {" "}
               <span className="font-bold hover:scale-105 transition-transform inline-block">
@@ -62,31 +62,32 @@ export default function Page() {
             to get started on your project today.
           </p>
         </div>
-        <div className="mb-6 relative ">
-          <Link href="/our_work/mmm-video">
-            <div className="flex flex-col justify-center items-center  absolute top-1/2  left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white hover:transform hover:scale-125 transition-transform">
-              <div>
-                <FaVideo size={36} />
+        <section id="our-work-video-section">
+          <div>
+            <Link href="/our_work/mmm-video">
+              <div className="flex flex-col justify-center items-center hover:transform hover:scale-110 transition-transform">
+                <div>
+                  <FaVideo size={36} />
+                </div>
+                <h3 className="text-4xl md:text-6xl font-bold">
+                  VIDEO
+                </h3>
               </div>
-              <h3 className="text-4xl md:text-6xl font-semibold z-50">VIDEO</h3>
-            </div>
-            <div className=" z-20">
-              <Image
-                src="/images/ourWorkPage/video_banner_image.png"
-                alt="MMM Video Production at work"
-                width={5008}
-                height={1996}
-                priority={true}
-              />
-            </div>
-          </Link>
-        </div>
-        <div className="md:text-lg flex justify-center items-center ">
-          <p className="p-2 text-center text-balance w-10/12 md:w-1/2 ">
-            We have more than one way to get you what you need. Let us know if
-            one of our other services fits your needs.
+            </Link>
+          </div>
+          <div className="md:text-lg flex justify-center items-center ">
+          <p className="p-2 text-center text-balance w-10/12 md:w-1/2">
+           Our video production is awesome beyond awesome and here in this text block we are going to tell you all about it using keywords that search engines like. lorem ipsum blah blah blah. 
           </p>
         </div>
+        </section>
+
+        {/* <div className="mt-12 md:text-lg flex justify-center items-center ">
+          <p className="p-2 text-center text-balance w-10/12 md:w-1/2 ">
+            We have more than one way to get you what you need. Let us know if
+            one of our other services fits your needs. 
+          </p>
+        </div> */}
         <div className="grid grid cols-3 md:grid-cols-3 mb-6 mt-4 md:mt-12 md:mb-12">
           <div className="flex justify-center items-center mt-2 md:mt-0 order-1">
             <Link href="/our_work/drone">
@@ -116,17 +117,18 @@ export default function Page() {
             </Link>
           </div>
         </div>
-        <div className="flex justify-center items-center">
-          <h4 className="text-xl font-bold">
-            Get started on your project today!
-          </h4>
-        </div>
-        <div className="flex justify-center items-center p-2 mt-6 mb-6 md:mb-12">
+        
+        <div className="flex justify-center items-center p-2 mt-6 mb-2">
           <div className="mt-2 flex justify-center">
             <ContactUsButton />
           </div>
         </div>
-        <div className="w-full md:hidden">
+        <div className="flex justify-center items-center mb-12">
+          <h4 className="text-xl md:text-2xl font-bold">
+            Get started on your project today!
+          </h4>
+        </div>
+        <div className="w-full">
           <MMMLineLogoBlack />
         </div>
       </main>
