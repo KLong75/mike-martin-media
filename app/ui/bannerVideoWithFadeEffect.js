62f80416739fa82a2c0a0e93c6d57c96c675905c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-02 17:49:05 -0600

    start fade half second later

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -13,7 +13,7 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
     }, 1000);
     const videoFadeTimer = setTimeout(() => {
       setShowVideo(true);
-    }, 0);
+    }, 500);
     return () => {
       clearTimeout(hideLogoTimer);
       clearTimeout(videoFadeTimer);
