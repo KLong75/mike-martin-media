55d4b2c5459746cfa212bd73214cbc1409cd7857
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-03 06:43:48 -0600

    adjust fade time

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -13,7 +13,7 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
     }, 1000);
     const videoFadeTimer = setTimeout(() => {
       setShowVideo(true);
-    }, 800);
+    }, 1000);
     return () => {
       clearTimeout(hideLogoTimer);
       clearTimeout(videoFadeTimer);
