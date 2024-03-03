f541fd17b6b9ea58796e6b436443b3431f1de94c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-03 09:38:14 -0600

    fade adjust longer

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -13,7 +13,7 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
     }, 1000);
     const videoFadeTimer = setTimeout(() => {
       setShowVideo(true);
-    }, 200);
+    }, 400);
     return () => {
       clearTimeout(hideLogoTimer);
       clearTimeout(videoFadeTimer);
@@ -24,9 +24,10 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
     <section className="grid grid-cols-1 gap-4 ">
      <div className={`relative flex justify-center transition-colors duration-16000 ease-in-out ${showVideo ? 'bg-white' : 'bg-black'}`}>
         <div
-          className={`banner-video-aspect-ratio-container w-full transition-opacity duration-4000 ease-in-out ${
+          className={`banner-video-aspect-ratio-container w-full transition-opacity duration-8000 ease-in-out ${
             showVideo ? "opacity-100" : "opacity-0"
           }`}
+          // try showVideo fade duration at 1200ms if this doesn't look right
           style={{
             paddingTop: "56.25%",
           }}>
