a6e26052588c1c38053ad676f40fa4296e3cf0f0
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-03 06:59:02 -0600

    start vid fade in a touch later

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -13,7 +13,7 @@ export default function BannerVideoWithFadeEffect({ src, title }) {
     }, 1000);
     const videoFadeTimer = setTimeout(() => {
       setShowVideo(true);
-    }, 0);
+    }, 400);
     return () => {
       clearTimeout(hideLogoTimer);
       clearTimeout(videoFadeTimer);
