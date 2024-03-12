db5de22846c2099f03ff670097cde257270a930b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-03-11 21:04:26 -0500

    vimeo preconnect

--- app/preload-resources.js
+++ app/preload-resources.js
@@ -1,11 +1,11 @@
 "use client";
 
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
 export function PreloadResources() {
-  ReactDOM.preconnect("https://player.vimeo.com");
-  ReactDOM.preconnect("https://i.vimeocdn.com");
-  ReactDOM.preconnect("https://f.vimeocdn.com");
+  ReactDOM.preconnect("https://player.vimeo.com", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://i.vimeocdn.com", { crossOrigin: "anonymous" });
+  ReactDOM.preconnect("https://f.vimeocdn.com", { crossOrigin: "anonymous" });
 
   return null;
 }
