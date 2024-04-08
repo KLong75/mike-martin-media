5c91ebb97178f5217bfe0e02845239dcc62d440e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-04-08 18:54:27 -0500

    reformat contact form validation

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,18 +1,18 @@
 export function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   
   return re.test(String(email).toLowerCase());
 }
 export function validateName(name) {
-  var re = /^[a-zA-Z]+$/;
+  const re = /^[a-zA-Z]+$/;
   return re.test(String(name));
 }
 export function validateMessage(message) {
   return message.length > 0;
 }
 export function validatePhone(phone) {
-  // Regular expression to match the 3 usual formats of US phone numbers
-  var re = /^(?:\(\d{3}\)\s?|\d{3}-)?\d{3}-?\d{4}$/;
+  // Regular expression to match phine number formats
+  const re = /^(?:\+1\s?|001\s?)?(?:\(\d{3}\)\s?|\d{3}[-.\s]?)?\d{3}[-.\s]?\d{4}$/;
   return phone === "" || re.test(String(phone));
 }
 export function validateForm(firstName, lastName, email, phone, message) {
@@ -20,16 +20,16 @@ export function validateForm(firstName, lastName, email, phone, message) {
 }
 
 export function formatDate(post_date) {
-  let date = new Date(post_date);
+  const date = new Date(post_date);
 
-  let monthNames = [
+  const monthNames = [
       'January', 'February', 'March', 'April', 'May', 'June',
       'July', 'August', 'September', 'October', 'November', 'December'
   ];
 
-  let day = date.getDate();
-  let monthIndex = date.getMonth();
-  let year = date.getFullYear();
+  const day = date.getDate();
+  const monthIndex = date.getMonth();
+  const year = date.getFullYear();
 
   return `${monthNames[monthIndex]} ${day}, ${year}`;
 }
