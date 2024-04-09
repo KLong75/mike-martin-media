a18be223e6d515bba09b098642b03dc5810efd03
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-04-08 19:15:56 -0500

    first name trim

--- app/ui/contactForm.js
+++ app/ui/contactForm.js
@@ -138,7 +138,7 @@ export default function ContactForm() {
           <input
             autoComplete="given-name"
             placeholder="First Name"
-            onChange={(e) => handleChange(e.target.value.trim(), setFirstName)}
+            onChange={(e) => handleChange(e.trim(), setFirstName)}
             value={firstName}
             required
             type="text"
