315c179470e1aedf455e6c6267dcd7779e77a737
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-08-20 12:05:42 -0500

    update

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' https://cdn-cookieyes.com https://www.googletagmanager.com https://www.gstatic.com https://www.gstatic.com/cv/js/sender/v1/cast_sender.js https://f.vimeocdn.com https://player.vimeo.com/* https://player.vimeo.com/video/911636273?h=f61fbbc179&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&controls=0&loop=1&background=1&title=0&muted=1&byline=0&portrait=0 https://*.vimeocdn.com https://*.vimeo.com;",
+            value: "script-src 'self' https://cdn-cookieyes.com https://www.googletagmanager.com https://www.gstatic.com https://www.gstatic.com/cv/js/sender/v1/cast_sender.js https://f.vimeocdn.com https://player.vimeo.com/ https://*.vimeocdn.com https://*.vimeo.com;",
           },
         ],
       },
