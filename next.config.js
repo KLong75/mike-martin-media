003a56b5260cfbbeeab6107beb3714af3222f150
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-08-20 10:52:27 -0500

    edit csp

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "script-src 'self' https://cdn-cookieyes.com https://www.googletagmanager.com https://www.gstatic.com https://www.gstatic.com/cv/js/sender/v1/cast_sender.js https://f.vimeocdn.com https://player.vimeo.com https://*.vimeocdn.com https://*.vimeo.com;",
+            value: "script-src 'self' 'unsafe-inline' https://cdn-cookieyes.com https://www.googletagmanager.com https://www.gstatic.com https://www.gstatic.com/cv/js/sender/v1/cast_sender.js https://f.vimeocdn.com https://player.vimeo.com https://*.vimeocdn.com https://*.vimeo.com;",
           },
         ],
       },
