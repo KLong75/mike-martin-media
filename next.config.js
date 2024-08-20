0e5635aea2dbaffac31fb4724702c74063e5b358
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-08-20 09:29:30 -0500

    remove placeholder from next config images

--- next.config.js
+++ next.config.js
@@ -17,12 +17,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co', 
-        port: '', // Leave empty if no specific port is required
-        pathname: '/**', // Allows any path
-      },
-      {
-        protocol: 'https',
         hostname: 'www.ziprecruiter.com', 
         port: '', // Leave empty if no specific port is required
         pathname: '/**', // Allows any path
