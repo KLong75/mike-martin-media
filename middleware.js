c629afc42df973f27b657b785613398937884acf
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-13 20:22:31 -0500

    create middleware.js

---
+++ middleware.js
@@ -0,0 +1,54 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' 'unsafe-eval'
+    https://cdn-cookieyes.com 
+    https://cdn-cookieyes.com/client_data/*/script.js 
+    https://www.googletagmanager.com 
+    https://www.gstatic.com 
+    https://www.gstatic.com/cv/js/sender/v1/cast_sender.js 
+    https://f.vimeocdn.com 
+    https://player.vimeo.com
+    https://cdn.lightwidget.com/widgets/lightwidget.js
+    https://cdn.lightwidget.com/widgets/fe8af16ea57f5ce0b3df76d3341327a5.html
+    https://va.vercel-scripts.com/v1/script.debug.js;;
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-src 'self' 
+      https://player.vimeo.com 
+      https://f.vimeocdn.com 
+      https://www.google.com; 
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
+
+  return response;
+}
--- next.config.js
+++ next.config.js
@@ -33,6 +33,47 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)", // Apply headers to all routes
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: `
+  //             default-src 'self';
+  //             script-src 'self' 'unsafe-inline' 'unsafe-eval'
+  //             https://cdn-cookieyes.com 
+  //             https://cdn-cookieyes.com/client_data/*/script.js 
+  //             https://www.googletagmanager.com 
+  //             https://www.gstatic.com 
+  //             https://www.gstatic.com/cv/js/sender/v1/cast_sender.js 
+  //             https://f.vimeocdn.com 
+  //             https://player.vimeo.com
+  //             https://cdn.lightwidget.com/widgets/lightwidget.js
+  //             https://cdn.lightwidget.com/widgets/fe8af16ea57f5ce0b3df76d3341327a5.html
+  //             https://va.vercel-scripts.com/v1/script.debug.js
+  //             ;
+  //             style-src 'self';
+  //             img-src 'self' data: https: https://i.vimeocdn.com https"//www.ziprecruiter.com;
+  //             font-src 'self';
+  //             connect-src 'self' https://player.vimeo.com https://f.vimeocdn.com;
+  //             frame-src https://player.vimeo.com/ https://f.vimeocdn.com
+  //             https://www.google.com; 
+  //             object-src 'none';
+  //             base-uri 'self';
+  //             form-action 'self';
+  //             frame-ancestors 'none';
+  //             upgrade-insecure-requests;
+  //             block-all-mixed-content;
+  //           `
+  //             .replace(/\s{2,}/g, " ")
+  //             .trim(), // Minify the CSP string
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
   images: {
     remotePatterns: [
       {
