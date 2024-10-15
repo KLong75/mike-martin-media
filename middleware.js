f4b3ae301e53286b0d435a1d13b83b4c6c565b4f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-15 09:16:04 -0500

    update csp matcher

--- middleware.js
+++ middleware.js
@@ -44,20 +44,21 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    {
-      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
-    },
-  ],
+  // matcher: [
+  //   /*
+  //    * Match all request paths except for the ones starting with:
+  //    * - api (API routes)
+  //    * - _next/static (static files)
+  //    * - _next/image (image optimization files)
+  //    * - favicon.ico (favicon file)
+  //    */
+  //   {
+  //     source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  //     missing: [
+  //       { type: "header", key: "next-router-prefetch" },
+  //       { type: "header", key: "purpose", value: "prefetch" },
+  //     ],
+  //   },
+  // ],
+  matcher: "/:path*",
 };
