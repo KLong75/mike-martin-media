aff509863a7c12bedec8b90b7b76ad6874cb7bf8
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-17 09:25:25 -0500

    disable vercel toolbar in preview

--- middleware.js
+++ middleware.js
@@ -26,8 +26,7 @@ export function middleware(request) {
 
   const previewCspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' https://player.vimeo.com/* https://vercel.live/_next-live/feedback/feedback.js https://vercel.live/_next-live/feedback/feedback.js https://vercel.live/_next-live/feedback/instrument.4d180d77d2ed5e14c59d.js;
-   
+    script-src 'self' 'nonce-${nonce}' https://player.vimeo.com/* ;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
