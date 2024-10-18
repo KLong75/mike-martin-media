38d14298ae81eed0907234330a68170e4bb4a252
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-17 21:11:38 -0500

    update dev csp

--- middleware.js
+++ middleware.js
@@ -32,7 +32,7 @@ export function middleware(request) {
     script-src-elem 'self' 'nonce-${nonce}' https://cdn.lightwidget.com/widgets/lightwidget.js;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: https://cdn.lightwidget.com/* https://i.vimeocdn.com/video/*;
-    media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/*;
+    media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/* https://player.vimeo.com/video/*;
     font-src 'self' data:;
     object-src 'none';
     base-uri 'self';
@@ -48,7 +48,7 @@ export function middleware(request) {
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' cdn-cookieyes.com ;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: https://cdn.lightwidget.com/* https://i.vimeocdn.com/video/* https://*.google-analytics.com https://*.googletagmanager.com;
-    media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/*;
+    media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/* https://player.vimeo.com/video/*;
     connect-src 'self' *.cookieyes.com cdn-cookieyes.com www.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com;
     font-src 'self' data:;
     object-src 'none';
