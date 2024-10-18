aa01485a9a3196dac7a10114f142e49663127e48
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-18 06:34:29 -0500

    update csp in preview

--- middleware.js
+++ middleware.js
@@ -29,7 +29,7 @@ export function middleware(request) {
   const previewCspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://player.vimeo.com/* https://f.vimeocdn.com/* https://f.vimeocdn.com/js_opt/modules/utils/vuid.min.js https://f.vimeocdn.com/p/4.37.12/js/player.module.js https://player.vimeo.com/* https://cdn.lightwidget.com/widgets/lightwidget.js ;
-    script-src-elem 'self' 'nonce-${nonce}' https://cdn.lightwidget.com/widgets/lightwidget.js https://www.googletagmanager.com/gtm.js?id=GTM-5W7VWSTB;
+    script-src-elem 'self' 'nonce-${nonce}' https://cdn.lightwidget.com/widgets/lightwidget.js https://www.googletagmanager.com/gtm.js?id=GTM-5W7VWSTB https://googletagmanager.com/*;
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: https://cdn.lightwidget.com/* https://i.vimeocdn.com/video/*;
     media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/* https://player.vimeo.com/video/*;
