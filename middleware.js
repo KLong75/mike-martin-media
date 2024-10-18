caab198ffabbf92871c922afec5f9ac812da5ee1
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-18 06:21:03 -0500

    add connect-src to preview

--- middleware.js
+++ middleware.js
@@ -33,6 +33,7 @@ export function middleware(request) {
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data: https://cdn.lightwidget.com/* https://i.vimeocdn.com/video/*;
     media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/* https://player.vimeo.com/video/*;
+    connect-src 'self' *.cookieyes.com cdn-cookieyes.com www.googletagmanager.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com www.googletagmanager.com;
     font-src 'self' data:;
     object-src 'none';
     base-uri 'self';
