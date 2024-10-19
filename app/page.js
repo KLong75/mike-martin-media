9ace19326857933fd7c6fc044bc1a891b2ec56b7
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-19 16:32:55 -0500

    update

--- app/page.js
+++ app/page.js
@@ -77,6 +77,7 @@ export default function Home() {
             className="flex justify-center items-center max-w-4xl mx-auto p-4 md:mt-4 home-page-banner-image-container"
           >
             <BannerImage
+              priority={}
               src={medicalVideoShoot}
               alt="The MMM Team shooting an onsite medical video"
             />
