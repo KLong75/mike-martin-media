860f80004b51415ea76cd03d45a9afc0a1061947
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-21 08:21:59 -0500

    start on active campaign form

---
+++ app/ui/activeCampaignContactForm.js
@@ -0,0 +1,16 @@
+import Script from "next/script";
+
+export default function ActiveCampaignContactForm() {}
+  return (
+    <>
+<div class="_form_1"></div>
+  <Script
+  src="https://mmmmarketing42.activehosted.com/f/embed.php?id=1"
+  
+  ch
+  >
+  </Script>
+
+    </>
+    
+  );
