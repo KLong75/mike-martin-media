a2d2c1a61f72e4f7196e21b5f90fb900f871caef
Kevin Long <98487770+KLong75@users.noreply.github.com>
2024-10-23 15:53:07 -0500

    update meta data to include www

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
   },
   description:
     "Mike Martin Media specializes in video production and creative services for corporations, nonprofits, education, and medical institutions. Our mission is to help our clients promote their causes, inform their clients, and inspire with their stories. We deliver dynamic multi-media solutions. Our team brings decades of combined experience from the broadcast and production industries to create high quality content for your organization.",
-  metadataBase: new URL("https://mikemartinmedia.com/"),
+  metadataBase: new URL("https://www.mikemartinmedia.com/"),
   alternates: {
     canonical: "/",
   },
