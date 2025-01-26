c69268ac382223bc09d2275070d0635392542478
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-01-26 11:38:04 -0600

    create empty inustry and capability pages

---
+++ app/capabilities/animation-motion-graphics/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Animation & Motion Graphics page</p>
+    </div>
+  );
+}
---
+++ app/industries/medical/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Medical</p>
+    </div>
+  );
+}
