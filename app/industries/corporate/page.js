a9f1c8ee0e5df0d096db43eca68bee4b589a484e
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-01-28 16:21:25 -0600

    start on industry pages

--- app/industries/corporate/page.js
+++ app/industries/corporate/page.js
@@ -1,3 +1,8 @@
+//import components
+import BannerVideoWithFadeNoLogo from "@/app/ui/bannerVideoWithFadeNoLogo";
+import VideoGallery from "@/app/ui/videoGallery";
+import CapabilitiesBanner from "@/app/ui/capabilitiesBanner";
+
 export default function Page() {
   return (
     <div>
