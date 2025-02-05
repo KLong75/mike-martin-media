25684e5b6d9947b157b6f88b371f7b8d54ebb30f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-02-04 21:18:15 -0600

    update

--- app/ui/pageHeadingBanner.js
+++ app/ui/pageHeadingBanner.js
@@ -3,7 +3,7 @@ export default function PageHeadingBanner({ title, heading, text }) {
     <div className="bg-black text-white flex justify-center">
       <div className="bg-black text-white w-full h-auto flex grid grid-cols-1 md:grid-cols-2 max-w-800 my-5">
         <div className="flex flex-col justify-center my-4 px-12 lg:ml-28 xl:ml-36 ">
-          <h1 className="text-xs sm:text-sm  font-semibold">
+          <h1 className="mb-1 text-xs sm:text-sm font-semibold">
             {title.toUpperCase()}
           </h1>
           <h2 className="text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-5xl 3xl:text-6xl font-semibold">
