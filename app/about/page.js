489785fa5d5ca4f48dada5852ba8b8192e7ed261
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-02-23 15:04:37 -0600

    rework about page

--- app/about/page.js
+++ app/about/page.js
@@ -12,8 +12,10 @@ import BannerImage from "../ui/bannerImage";
 import ContactUsButton from "../ui/contactUsButton";
 import BannerVideoWithFadeNoLogo from "../ui/bannerVideoWithFadeNoLogo";
 import MMMLineLogoHeading from "../ui/mmmLineLogoHeading";
+import MMMTeamPhotoWithLogo from "../ui/mmmTeamPhotoWithLogo";
 // iport images
 import mikeInVanWithCamera from "../../public/images/about-page/mike-in-van-with-camera.jpg";
+import mikeVan from "../../public/images/about-page/mike-van.png";
 // import text
 import { aboutPageText } from "../lib/siteText";
 import PageHeadingBanner from "../ui/pageHeadingBanner";
@@ -42,91 +44,87 @@ export default function Page() {
           heading={"Turning Your Ideas Into Impactful Stories"}
           text={
             <>
-            At MMM, we create video content{" "}
-            <Link 
-              href="/capabilities/video-production"
-              className="underline"
-            >
-              video content
-            </Link>{" "}
-            that not only looks great but also makes a positive impact. We believe in the power of telling stories to inspire, connect and make real change. Every project we complete is an opportunity to help our clients succeed while contributing something good to the world.
-          </>
+              At MMM, we create video content{" "}
+              <Link href="/capabilities/video-production" className="underline">
+                video content
+              </Link>{" "}
+              that not only looks great but also makes a positive impact. We
+              believe in the power of telling stories to inspire, connect and
+              make real change. Every project we complete is an opportunity to
+              help our clients succeed while contributing something good to the
+              world.
+            </>
           }
         />
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 p-6 mt-2 md:mt-6">
-        <div className="flex justify-center items-center">
-          <div className="p-2 md:p-6 ">
-            <BannerImage
-              src={mikeInVanWithCamera}
-              alt="Mike Martin in a white van with a video camera"
-              priority
-            />
+      <section className="flex justify-center items-center p-6 md:p-12 lg:p-28 xl:p-42">
+        <div
+          id="home-page-about-section"
+          className="grid grid-cols-1 xl:grid-cols-2 gap-4 lg:gap-12 xl:gap-16 max-w-800 flex justify-center items-center">
+          <div className="order-2 xl:order-1 relative flex justify-center items-center">
+            <MMMTeamPhotoWithLogo />
           </div>
-        </div>
-        <div className="grid grid-cols-1">
-          <div className="flex justify-center items-center mt-6 mb-6 sm:mb-12 3xl:-mb-6">
-            <p
-              className="md:text-2xl xl:text-2xl 3xl:text-4xl text-center lg:text-left text-balance w-10/12"
-              id="about-text">
-              {aboutPageText.about_text_a}
+
+          <div className="flex flex-col justify-left order-1 xl:order-2 max-w-800">
+            <h3 className="font-semibold text-2xl lg:text-3xl xl:text-4xl">
+              Who We Are
+            </h3>
+            <p className="md:text-lg xl:text-xl text-balance my-4 lg:my-6">
+            We are a team of creatives who found our niche in producing meaningful, high-quality video content. We understand that your story is unique and feel it should be told in a way that resonates with your audience. From concept to the final product, we are dedicated to crafting videos that are authentic, impactful and aligned with your goals.
             </p>
-          </div>
-          <div className="flex justify-center items-center sm:mb-12 mt-12">
-            <p
-              className="md:text-2xl xl:text-2xl 3xl:text-4xl text-center lg:text-left text-balance w-10/12"
-              id="about-text">
-              {aboutPageText.about_text_b}
+            <p className="md:text-lg xl:text-xl text-balance my-4 lg:my-6">
+            While we do love to have fun, we also take our work very seriously—because we know that when it’s done right, video can move people, inspire action and make a difference.
             </p>
+      
           </div>
-        </div>
-      </div>
-      <div className="flex justify-center mt-4 mb-6">
-        <ContactUsButton />
-      </div>
-      <section className="md:mt-12">
-        <div className="grid grid-cols-1 lg:grid-cols-3 w-full mt-6">
-          {weTextBlockData.map((block) => (
-            <WeTextBlock
-              key={block.headline}
-              headline={block.headline}
-              text={block.text}
-            />
-          ))}
         </div>
       </section>
       <section className="mt-8 md:mt-12">
-      <div className="">
+        <div className="lg:px-6 2xl:px-0">
           <MMMLineLogoHeading headline="Meet The Team" heading_level={"h2"} />
         </div>
-        {/* <div className="bg-black text-white flex justify-center items-center w-full h-20 md:h-24">
-          <div className="w-8 md:w-10 h-auto mx-2 meet-the-team-m-logo-container">
-            <Image
-              src="/branding/singleMWhiteResizeSmall.png"
-              width={349}
-              height={271}
-              alt="M"
-            />
+        <div className="flex justify-center">
+          <div className="max-w-800">
+            <TeamGallery />
           </div>
-          <h2 className="text-3xl md:text-4xl font-bold -ml-2" id="mmm-team">
-            eet The Team
-          </h2>
-        </div> */}
-        {/* <div className="flex justify-center items-center mb-4 mt-2 md:mt-4 md:mb-8 ">
-          <p className="md:text-lg xl:text-xl 3xl:text-2xl p-6 text-center text-balance xl:mt-6 xl:mb-6">
-            {aboutPageText.meet_the_team_text}
-          </p>
-        </div> */}
-        <div>
-          <TeamGallery />
         </div>
       </section>
-      {/* <section className="-mt-6 md:mt-0 md:-mb-20">
-        <InstagramWidget />
-      </section> */}
-      {/* <div className="w-full">
-        <MMMLineLogoBlack />
-      </div> */}
+      <div className="flex justify-center items-center p-8 lg:p-12">
+        <div className="grid grid-cols-1 xl:grid-cols-2 max-w-600 gap-6">
+          <div className="mb-6">
+            <h6 className="text-2xl lg:text-4xl font-bold mb-2 lg:mb-6">
+              Our Journey
+            </h6>
+            <div>
+              <p className="lg:text-xl xl:text-balance">
+                MMM was born out of a desire to do things differently. When we
+                started, we knew we wanted to be more than just a production
+                company. We wanted to create something that made a tangible,
+                positive difference. We found our niche by focusing on quality,
+                efficiency and impact, and help our clients bring their visions
+                to life in a way that matters.
+              </p>
+            </div>
+            <div className="mt-6">
+              <p className="lg:text-xl xl:text-balance">
+                We've built lasting relationships with clients who share our
+                values, and we've helped them achieve success while also making
+                the world a little better along the way. We're proud of where we
+                started, and even more excited for where we're going.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center">
+            <Image
+              src={mikeVan}
+              alt="Mike Martin in a white van with a video camera"
+              priority
+              width={708}
+              height={5386}
+            />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
