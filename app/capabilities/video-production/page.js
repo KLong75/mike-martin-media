25ec16684b6bea58daa43419d7409aa097f78fbe
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-06-03 18:21:24 -0500

    update

--- app/capabilities/video-production/page.js
+++ app/capabilities/video-production/page.js
@@ -17,7 +17,8 @@ import {
   exampleVideoProductionVideoSrc,
 } from "../../lib/capabilities/video-production.js";
 
-const videoProductionPageOgImgUrl = "https://www.mikemartinmedia.com/images/open-graph/video-production-og.png";
+const videoProductionPageOgImgUrl =
+  "https://www.mikemartinmedia.com/images/open-graph/video-production-og.png";
 
 export const metadata = {
   title: "Business Video Production | Mike Martin Media",
@@ -40,7 +41,7 @@ export const metadata = {
         width: 1200,
         height: 630,
       },
-        // {
+      // {
       //   url: ,
       //   width: 1920,
       //   height: 1080,
@@ -69,11 +70,11 @@ export const metadata = {
         width: 1200,
         height: 630,
       },
-        // {
-        //   url: ,
-        //   width: 800,
-        //   height: 418,
-        // },
+      // {
+      //   url: ,
+      //   width: 800,
+      //   height: 418,
+      // },
     ],
   },
 };
@@ -85,10 +86,11 @@ export default function Page() {
         src="https://player.vimeo.com/video/911636273?h=f61fbbc179&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&controls=0&loop=1&background=1&title=0&muted=1&byline=0&portrait=0"
         title="Mike Martin Media: St. Louis Video Production Company"
       /> */}
+      {/* comment */}
       <BannerVideoWithFadeNoLogo
-            src="https://player.vimeo.com/video/938224537?h=7b77483940&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&controls=0&loop=1&background=1&title=0&muted=1&byline=0&portrait=0"
-            title="Mike Martin Media | Our Work"
-          />
+        src="https://player.vimeo.com/video/938224537?h=7b77483940&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&controls=0&loop=1&background=1&title=0&muted=1&byline=0&portrait=0"
+        title="Mike Martin Media | Our Work"
+      />
       <div className="">
         <PageHeadingBanner
           title={videoProductionPageHeadingBannerText.title}
