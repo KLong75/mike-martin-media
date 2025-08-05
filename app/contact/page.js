a49e69af4e6bf03f21b8ad6dd3cd5a143233c522
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-05 15:40:17 -0500

    update

--- app/contact/page.js
+++ app/contact/page.js
@@ -100,7 +100,7 @@ export default function Page() {
 
       {/* <ActiveCampaignContactForm /> */}
       <div>
-        <ContactFormWrapper />
+        {/* <ContactFormWrapper /> */}
       </div>
     </main>
   );
