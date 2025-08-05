239832c564df196ef531a6f26bf87242894df958
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-05 17:38:35 -0500

    update csp

--- middleware.js
+++ middleware.js
@@ -43,10 +43,10 @@ export function middleware(request) {
 
   const productionCspHeader = `
     default-src 'none';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://f.vimeocdn.com/* https://f.vimeocdn.com/js_opt/modules/utils/vuid.min.js https://f.vimeocdn.com/p/4.37.12/js/player.module.js https://player.vimeo.com/* https://cdn.lightwidget.com/widgets/lightwidget.js cdn-cookieyes.com https://mmmmarketing42.activehosted.com;
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://f.vimeocdn.com/* https://f.vimeocdn.com/js_opt/modules/utils/vuid.min.js https://f.vimeocdn.com/p/4.37.12/js/player.module.js https://player.vimeo.com/* https://cdn.lightwidget.com/widgets/lightwidget.js cdn-cookieyes.com https://mmmmarketing42.activehosted.com https://*.googletagmanager.com;
     script-src-elem 'self' 'nonce-${nonce}' https://www.googletagmanager.com/gtm.js/* https://googletagmanager.com/*  https://mmmmarketing42.activehosted.com/ https://prism.app-us1.com/ https://cdn-cookieyes.com/ ;
-    style-src 'self' 'unsafe-inline' https://fonts.bunny.net/css;
-    img-src 'self' blob: data: https://i.vimeocdn.com/video/* https://cdn-cookieyes.com/assets/images/close.svg https://cdn-cookieyes.com/assets/images/poweredbtcky.svg cdn-cookieyes.com https://d226aj4ao1t61q.cloudfront.net/esfkyjh1u_forms-close-dark.png https://www.googletagmanager.com/td https://www.googletagmanager.com/a https://www.google.com/pagead/;
+    style-src 'self' 'unsafe-inline' https://fonts.bunny.net/css https://www.googletagmanager.com https://fonts.googleapis.com;
+    img-src 'self' blob: data: https://i.vimeocdn.com/video/* https://cdn-cookieyes.com/assets/images/close.svg https://cdn-cookieyes.com/assets/images/poweredbtcky.svg cdn-cookieyes.com https://d226aj4ao1t61q.cloudfront.net/esfkyjh1u_forms-close-dark.png https://www.googletagmanager.com/td https://www.googletagmanager.com/a https://www.google.com/pagead/ https://*.google-analytics.com https://*.googletagmanager.com fonts.gstatic.com;
     media-src 'self' blob: data: https://player.vimeo.com https://f.vimeocdn.com https://vimeo.com https://i.vimeocdn.com/video/* https://player.vimeo.com/video/* https://scontent-hel3-1.cdninstagram.com/o1/;
     connect-src 'self' 'nonce-${nonce}' https://analytics.google.com *.cookieyes.com cdn-cookieyes.com https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com www.googletagmanager.com https://mmmmarketing42.activehosted.com/proc.php https://process.iconnode.com/google-ads/ https://*.ksrndkehqnwntyxlhgto.com https://www.google-analytics.com https://stats.g.doubleclick.net https://www.google.com https://google.com;
     font-src 'self' data: https://fonts.bunny.net/; 
@@ -57,7 +57,6 @@ export function middleware(request) {
     frame-src 'self' https://player.vimeo.com https://f.vimeocdn.com https://www.google.com https://stats.g.doubleclick.net https://td.doubleclick.net/ https://www.googletagmanager.com/;
     upgrade-insecure-requests; 
   `;
-
   if (isDevelopment) {
     cspHeader = developmentCspHeader;
   } else if (isPreview) {
