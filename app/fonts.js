078f897ab22782b98e9d2ac8d98e54d3adad968f
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-08-05 19:42:30 -0500

    update font logic

--- app/fonts.js
+++ app/fonts.js
@@ -2,5 +2,9 @@
 import { DM_Sans } from "next/font/google";
 // import { DM_Serif_Display } from "next/font/google";
 
-export const dm_sans = DM_Sans({subsets: ["latin"], weight: '400'});
+export const dm_sans = DM_Sans({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-dm-sans",
+});
 // export const dm_serif_display = DM_Serif_Display({ subsets: ["latin"], weight: '400'});
