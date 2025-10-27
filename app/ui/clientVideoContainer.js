0e069887c213818e1720590cccc5d76e7f66cd3d
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-10-26 23:07:52 -0500

    set muted to false on portfolio vids

--- app/ui/clientVideoContainer.js
+++ app/ui/clientVideoContainer.js
@@ -55,7 +55,7 @@ export default function ClientVideoContainer({ video }) {
       title={video.title}
       autoPlay={false}
       loop={false}
-      // muted={false}
+      muted={false}
       poster={video.image_src}
       disableCookies={true}
       disableTracking={true}
