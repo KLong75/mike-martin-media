046ec9c3a771f75a2f8939536f97050853ae4d2a
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-10-28 12:53:39 -0500

    resize team photo

--- app/ui/mmmTeamPhotoWithLogo.js
+++ app/ui/mmmTeamPhotoWithLogo.js
@@ -6,9 +6,9 @@ export default function MMMTeamPhotoWithLogo() {
     <div className="relative w-full">
       <Image
         // priority
-        src="/images/home-page/mmm-team-big.png"
-        width={819}
-        height={546}
+        src="/images/home-page/mmm-team-900x600.webp"
+        width={900}
+        height={600}
         alt="The MMM Team"
         aria-label="The MMM Team"
         className="w-full h-auto"
