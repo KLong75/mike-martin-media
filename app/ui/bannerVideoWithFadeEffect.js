48afdea0caf1e823f4ffa24d7a0da357e6d0e3d2
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-10-28 21:23:34 -0500

    update

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -105,7 +105,6 @@ export default function BannerVideoWithFadeEffect({
               hideLogo ? "opacity-0" : "opacity-100"
             }`}>
             <Image
-              fetchPriority="high"
               priority
               src={whiteMmmLogo}
               alt="MMM logo"
