0c95505ed35fc2316ca4e82017ee29751440d113
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-10-29 10:07:04 -0500

    update

--- app/ui/bannerVideoWithFadeEffect.js
+++ app/ui/bannerVideoWithFadeEffect.js
@@ -19,7 +19,7 @@ export default function BannerVideoWithFadeEffect({
   poster
 }) {
   const [hideLogo, setHideLogo] = useState(false);
-  const [showVideo, setShowVideo] = useState(true);
+  const [showVideo, setShowVideo] = useState(false);
   const [showHeading, setShowHeading] = useState(false);
   const [letters, setLetters] = useState([]);
 
