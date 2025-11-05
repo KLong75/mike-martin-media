9f67676ba099fbd402a288bb705850edc0e8391b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2025-11-05 12:52:59 -0600

    hide signup form on mobile until btn click

--- app/ui/newsletterSignupButton.js
+++ app/ui/newsletterSignupButton.js
@@ -1,6 +1,6 @@
 export default function NewsletterSignupButton() {
   const handleClick = () => {
-    console.log('Newsletter signup button clicked');
+    console.log("Newsletter signup button clicked");
     // Check for the cookie named '_form_3_'
     const cookieName = "_form_3_";
     const cookies = document.cookie.split(";");
@@ -21,13 +21,13 @@ export default function NewsletterSignupButton() {
     // if the script already exists, remove it
     const existingScript = document.getElementById("active-campaign-script");
     if (existingScript) {
-      console.log('Removing existing Active Campaign script');
+      console.log("Removing existing Active Campaign script");
       existingScript.remove();
     }
 
-     const existingFormWrapper = document.querySelector("._form-wrapper");
+    const existingFormWrapper = document.querySelector("._form-wrapper");
     if (existingFormWrapper) {
-      console.log('Removing existing form wrapper');
+      console.log("Removing existing form wrapper");
       existingFormWrapper.remove();
     }
 
@@ -43,16 +43,21 @@ export default function NewsletterSignupButton() {
     //   existingForm.remove();
     // }
 
-   
-
     // Append the script to the document body
     document.body.appendChild(script);
+
+    setTimeout(() => {
+      const formWrapper = document.querySelector("._form-wrapper");
+      if (formWrapper) {
+        formWrapper.style.display = "block";
+      }
+    }, 200); // Wait for the form to be injected
   };
 
   return (
     <button
       id="newsletter-signup-button"
-      className="bg-white text-black font-bold rounded-full px-2 py-1 mt-2 text-sm hover:scale-105 transition duration-500 ease-in-out"
+      className="bg-white text-black font-bold rounded-full px-2 py-1 mt-2 text-sm md:hover:scale-105 transition duration-500 ease-in-out"
       onClick={handleClick}>
       Subscribe to our Newsletter
     </button>
