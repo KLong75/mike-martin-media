eb22f660948b0f05e07db4f12bce240643eb4e32
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-01-26 13:48:34 -0600

    make updates from rachels notes

--- app/ui/capabilityBlockContainer.js
+++ app/ui/capabilityBlockContainer.js
@@ -2,11 +2,13 @@
 import CapabilityBlock from "./capabilityBlock";
 //import data
 import { capabilities } from "../lib/capabilities/capabilities";
+//filter out Product Photography capability
+const filteredCapabilities = capabilities.filter((capability) => capability.label !== "Product Photography");
 
 export default function CapabilityBlockContainer() {
   return (
     <div className="flex grid grid-cols-2 xl:grid-cols-3 gap-1">
-      {capabilities.map((capability, index) => (
+      {filteredCapabilities.map((capability, index) => (
         <CapabilityBlock
           key={index}
           label={capability.short_label}
