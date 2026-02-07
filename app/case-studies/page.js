422df82b269054944017f07ea21d5c2c9f47766c
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-02-06 19:03:40 -0600

    update

--- app/case-studies/page.js
+++ app/case-studies/page.js
@@ -107,7 +107,7 @@ export default function Page() {
         />
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 px-2 md:px-6 lg:px-12 py-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-6 px-2 md:px-6 lg:px-12 py-6 max-w-800 mx-auto">
         {caseStudies.map((caseStudy) => (
           <CaseStudyPreview key={caseStudy.slug} caseStudy={caseStudy} />
         ))}
