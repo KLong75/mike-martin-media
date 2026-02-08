72d051dbd2790f67b77daec30f7fe40f6db6169b
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-02-07 22:23:50 -0600

    update

--- app/ui/case-study-preview.js
+++ app/ui/case-study-preview.js
@@ -1,6 +1,10 @@
 // import from next
 import Link from "next/link";
 import Image from "next/image";
+// import components
+import MMMLineLogoBlack from "../ui/mmmLineLogoBlack";
+// import MMMLineLogoHeading from "../ui/mmmLineLogoHeading";
+import clsx from "clsx";
 // import from utils
 import { shortenPostText } from "@/utils/utils";
 
@@ -10,12 +14,21 @@ export default function CaseStudyPreview({ caseStudy }) {
       {caseStudy ? (
         <Link href={`/case-studies/study/${caseStudy.slug}`}>
           <div className="p-2 bg-neutral-200 md:hover:bg-black md:hover:text-white transition-colors duration-1000 ease-in-out group shadow-2xl md:shadow-none rounded-2xl md:rounded-none flex flex-col">
-          <div className="mx-auto mb-2">
-            <h3 className="text-center text-xl font-semibold my-1">{caseStudy.title}</h3>
-            <h4><span className="font-bold">Client:</span> {caseStudy.client}</h4>
-            <h5><span className="font-bold">Campaign:</span> {caseStudy.campaign}</h5>
-            <h6><span className="font-bold">Format:</span> {caseStudy.format}</h6>
-          </div>
+            <div className="mx-auto mb-2">
+              <h3 className="text-center text-xl font-semibold my-1">
+                {caseStudy.title}
+              </h3>
+              <h4>
+                <span className="font-bold">Client:</span> {caseStudy.client}
+              </h4>
+              <h5>
+                <span className="font-bold">Campaign:</span>{" "}
+                {caseStudy.campaign}
+              </h5>
+              <h6>
+                <span className="font-bold">Format:</span> {caseStudy.format}
+              </h6>
+            </div>
             <div className="flex justify-center w-full h-auto px-4">
               <Image
                 className="shadow-xl"
@@ -27,10 +40,20 @@ export default function CaseStudyPreview({ caseStudy }) {
               />
             </div>
             <div className="p-4">
-              {Array.isArray(caseStudy.challenge)
-                ? <p><span className="font-bold">The Challenge:</span> {shortenPostText(caseStudy.challenge[0], 200)}</p>
-                : <p><span className="font-span">The Challenge:</span> {shortenPostText(caseStudy.challenge, 200)}</p>
-              }
+              {Array.isArray(caseStudy.challenge) ? (
+                <p>
+                  <span className="font-bold">The Challenge:</span>{" "}
+                  {shortenPostText(caseStudy.challenge[0], 200)}
+                </p>
+              ) : (
+                <p>
+                  <span className="font-span">The Challenge:</span>{" "}
+                  {shortenPostText(caseStudy.challenge, 200)}
+                </p>
+              )}
+            </div>
+            <div>
+              <MMMLineLogoBlack />
             </div>
           </div>
         </Link>
