b96cad905ec7600a1d6f631ef2c5732cd0d760f6
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-03-26 09:19:30 -0500

    update photo gallery

--- app/ui/portfolioPhotoGallery.js
+++ app/ui/portfolioPhotoGallery.js
@@ -25,10 +25,8 @@ export default function PortfolioPhotoGallery({ client }) {
   const clientPhotoData = photographyWorkSampleData.filter(
     (data) => data.client === client,
   );
-  // console.log("Client Photo Data:", clientPhotoData);
+  
   const clientImages = clientPhotoData[0].images;
-  // console.log("Client Images:", clientImages);
-  // const isPrasino = client === "Prasino";
 
   // Disable scroll and pause autoplay when fullscreen
   useEffect(() => {
@@ -124,7 +122,7 @@ export default function PortfolioPhotoGallery({ client }) {
               >
                 <Image
                   src={image.src}
-                  alt={image.alt || "Gallery image"}
+                  alt={image.alt || `${client} photo gallery image by MMM`}
                   width={imageWidth}
                   height={imageHeight}
                   priority
@@ -155,7 +153,7 @@ export default function PortfolioPhotoGallery({ client }) {
           onClick={() => setFullScreenImage(null)}>
           <Image
             src={fullScreenImage.src}
-            alt={fullScreenImage.alt || "Gallery image"}
+            alt={fullScreenImage.alt || `${client} Gallery image`}
             width={clientPhotoData[0].width}
             height={clientPhotoData[0].height}
             priority
@@ -168,76 +166,3 @@ export default function PortfolioPhotoGallery({ client }) {
     </section>
   );
 }
-
-// "use client";
-// import { useState } from "react";
-// import { track } from "@vercel/analytics/react";
-// import Image from "next/image";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Autoplay, EffectFade, Pagination, Navigation } from "swiper/modules";
-// import "swiper/css";
-// import "swiper/css/effect-fade";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-// import { photographyWorkSampleData } from "@/app/lib/data";
-
-// export default function PortfolioPhotoGallery({ client }) {
-//   const [fullscreenIndex, setFullscreenIndex] = useState(null);
-//   const clientPhotoData = photographyWorkSampleData.filter(
-//     (data) => data.client === client
-//   );
-//   const clientImages = clientPhotoData[0].images;
-//   const isPrasino = client === "Prasino";
-
-//   return (
-//     <section className="text-center mx-auto w-full p-6 pb-0 md:p-12 md:pb-0 flex flex-col justify-center items-center">
-//       <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl font-bold">
-//         {`${clientPhotoData[0].client} Photo Gallery`}
-//       </h1>
-//       <Swiper
-//         className="photo-swiper"
-//         spaceBetween={30}
-//         loop={true}
-//         effect={"fade"}
-//         navigation={{ clickable: true }}
-//         pagination={{ clickable: true }}
-//         autoplay={{ delay: 3000, disableOnInteraction: false }}
-//         modules={[Autoplay, EffectFade, Navigation, Pagination]}
-//       >
-//         {clientImages.map((image, index) => (
-//           <SwiperSlide key={index}>
-//             <div
-//               className={isPrasino ? "w-full p-4" : "w-144 h-auto mx-auto"}
-//               style={{ cursor: "pointer" }}
-//               onClick={() => setFullscreenIndex(index)}
-//             >
-//               <Image
-//                 src={image.src}
-//                 alt={image.alt || "Gallery image"}
-//                 width={clientPhotoData[0].width}
-//                 height={clientPhotoData[0].height}
-//                 priority
-//                 className="p-12"
-//               />
-//             </div>
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//       {fullscreenIndex !== null && (
-//         <div
-//           className="fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50"
-//           onClick={() => setFullscreenIndex(null)}
-//         >
-//           <Image
-//             src={clientImages[fullscreenIndex].src}
-//             alt={clientImages[fullscreenIndex].alt || "Gallery image"}
-//             width={clientPhotoData[0].width}
-//             height={clientPhotoData[0].height}
-//             priority
-//             className="max-w-full max-h-full"
-//           />
-//         </div>
-//       )}
-//     </section>
-//   );
-// }
