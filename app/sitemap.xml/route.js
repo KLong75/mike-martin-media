c6a2778429ef8775e24b7c3dc63cfab7fa9a8ef0
Kevin Long <98487770+KLong75@users.noreply.github.com>
2026-05-04 10:16:51 -0500

    start sitemap update

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -1,5 +1,14 @@
 import sitemap from "../sitemap-config";
 
+function escapeXml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
 function generateSitemapXml(urls) {
   return `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
